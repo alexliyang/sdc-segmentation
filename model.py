bfaa588c7afc565676766d5e3ebc92f7a50f3b61
dzorlu <deniz@monahq.com>
2017-09-06 00:21:48 -0400

    encode continue

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 
 import os
 import sys
+import numpy as np
 
 import tensorflow as tf
 import utils
@@ -46,11 +47,81 @@ class SlimModelEncoder(object):
 		return init_fn, end_points
 
 class FCNDecoder(object):
-	def __init__(self, layers_to_connect):
-		self.layers_to_connect = layers_to_connect
+	def __init__(self, tensors_to_connect, input_tensor, nb_classes, scope):
+		self.tensors_to_connect = tensors_to_connect
+		self.input_tensor = input_tensor
+		self.nb_classes = nb_classes
+		self.scope = scope
+
+	# def upsample(layer, nb_classes):
+	# 	output = tf.layers.conv2d_transpose(layer, nb_classes, kernel_size=4, strides=(2, 2),
+	# 	                                    padding='same',
+	# 	                                    kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+	# 		return output
+
+	def _compute_strides(self, end_points):
+		nb_upsample_filters = len(self.tensors_to_connect)
+		map_size_w = np.zeros(nb_upsample_filters)
+		map_size_h = np.zeros(nb_upsample_filters)
+		stride_w = np.zeros(nb_upsample_filters)
+		stride_h = np.zeros(nb_upsample_filters)
+		for i in range(nb_upsample_filters):
+			map_size_h[i] = end_points[self.tensors_to_connect[i]].get_shape().as_list()[2]
+			map_size_w[i] = end_points[self.tensors_to_connect[i]].get_shape().as_list()[3]
+			stride_h[i] = map_size_h[i] / map_size_h[i-1] if i > 0 else None
+			stride_w[i] = stride_w[i] / stride_w[i - 1] if i > 0 else None
+		stride_w = stride_w[~np.isnan(stride_w)]
+		stride_h = stride_h[~np.isnan(stride_h)]
+		return stride_w, stride_h
+
+	# def get_deconv_filter(self, f_shape):
+	# 	# bilinear upsampling initializer
+	# 	width = f_shape[0]
+	# 	heigh = f_shape[0]
+	# 	f = ceil(width / 2.0)
+	# 	c = (2 * f - 1 - f % 2) / (2.0 * f)
+	# 	bilinear = np.zeros([f_shape[0], f_shape[1]])
+	# 	for x in range(width):
+	# 		for y in range(heigh):
+	# 			value = (1 - abs(x / f - c)) * (1 - abs(y / f - c))
+	# 			bilinear[x, y] = value
+	# 	weights = np.zeros(f_shape)
+	# 	for i in range(f_shape[2]):
+	# 		weights[:, :, i, i] = bilinear
+	#
+	# 	init = tf.constant_initializer(value=weights,
+	# 	                               dtype=tf.float32)
+	# 	var = tf.get_variable(name="up_filter", initializer=init,
+	# 	                      shape=weights.shape)
+	# 	return var
 
 	def build(self, end_points):
-		pass
+		stride_w, stride_h = self._compute_strides(end_points)
+
+		with tf.variable_scope(self.scope, 'fcn', [list(end_points.values())]) as sc:
+			end_points_collection = sc.name + '_end_points'
+			# Collect outputs for conv2d, fully_connected and max_pool2d.
+			with slim.arg_scope([slim.conv2d, slim.fully_connected, slim.max_pool2d],
+			                    weights_regularizer=slim.l2_regularizer(1e-4),
+			                    weights_initializer=slim.l2_regularizer(1e-4),
+			                    outputs_collections=end_points_collection):
+			for i in range(self.nb_upsample_filters-1):
+				slim.conv2d(end_points[self.tensors_to_connect[i]],
+				            self.nb_classes,
+				            [stride_h, stride_w],
+				            padding='same')
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
