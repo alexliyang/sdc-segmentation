9e959d3e1d9182d84b0d55e3b8e31f35f392157b
dzorlu <deniz@monahq.com>
2017-09-19 00:00:42 -0400

    adek20 dataset

--- train_adek20.py
+++ train_adek20.py
@@ -14,23 +14,23 @@ def main(_):
   iterator, filename = get_train_inputs(batch_size=1,
 	                                      repeat=True,
 	                                      num_classes=1)
-	# Model
-	encoder = SlimModelEncoder(name="vgg_16", num_classes=NB_CLASSES, is_training=True)
-	image, label = iterator.get_next()
-	image = tf.to_float(image)
-	restore_fn, end_points = encoder.build(image=image)
-	decoder = FCNDecoder(end_points, nb_classes=NB_CLASSES, scope='decoder')
 
-	tensors_to_connect = OrderedDict()
-	tensors_to_connect["vgg_16/fc8"] = (2,2)
-	tensors_to_connect['vgg_16/pool4'] = (2,2)
-	tensors_to_connect['vgg_16/pool3'] = (8,8)
-	net = decoder.build(tensors_to_connect)
+  encoder = SlimModelEncoder(name="vgg_16", num_classes=NB_CLASSES, is_training=True)
+  image, label = iterator.get_next()
+  image = tf.to_float(image)
+  restore_fn, end_points = encoder.build(image=image)
+  decoder = FCNDecoder(end_points, nb_classes=NB_CLASSES, scope='decoder')
 
-	# Train
-	trainer = Trainer(nb_classes=NB_CLASSES, optimizer=tf.train.AdamOptimizer, learning_rate=1e-4)
-	trainer.build(predictions=net, labels=label)
-	trainer.train(iterator,
+  tensors_to_connect = OrderedDict()
+  tensors_to_connect["vgg_16/fc8"] = (2,2)
+  tensors_to_connect['vgg_16/pool4'] = (2,2)
+  tensors_to_connect['vgg_16/pool3'] = (8,8)
+  net = decoder.build(tensors_to_connect)
+
+  # Train
+  trainer = Trainer(nb_classes=NB_CLASSES, optimizer=tf.train.AdamOptimizer, learning_rate=1e-4)
+  trainer.build(predictions=net, labels=label)
+  trainer.train(iterator,
 	              restore_fn=restore_fn,
 	              number_of_steps=2500,
 	              filename=['data/adek20_training.tfecord'])
